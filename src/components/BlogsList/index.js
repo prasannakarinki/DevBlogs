911c8a35deb0995ef8e6f0f2098ea05a9d36abb4
prasannakarinki <krishnaprasaannakarinki@gmail.com>
2023-01-27 05:45:43 +0000

    dev blogs

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,67 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+// const blogsData = [
+//   {
+//     id: 1,
+//     title: 'Blog 1',
+//     imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
+//     avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//     author: 'Author Name',
+//     topic: 'React.js',
+//   },
+//   {
+//     id: 2,
+//     title: 'Blog 2',
+//     imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
+//     avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//     author: 'Author Name',
+//     topic: 'React.js',
+//   },
+// ]
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      topic: eachItem.topic,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatart_url,
+      author: eachItem.author,
+    }))
+    this.setState({blogsData: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+        {blogsData.map(item => (
+          <BlogItem blogData={item} key={item.id} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
